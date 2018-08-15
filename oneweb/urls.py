c0f15a01cdeeaa44c3aab6340df98438ba073857
Sanjit Majumdar <sanjit.majumdar.201>
2018-08-15 14:30:39 +0600

    about

--- oneweb/urls.py
+++ oneweb/urls.py
@@ -20,5 +20,6 @@ from oneweb import views
 
 urlpatterns = [
     path('', views.HomePage.as_view(), name='home'),
+    path('about/',views.About.as_view(), name='about'),
     path('admin/', admin.site.urls),
 ]
